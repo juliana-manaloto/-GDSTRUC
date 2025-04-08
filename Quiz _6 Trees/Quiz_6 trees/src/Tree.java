f73e7da9e49f7c6341c172fca4e019e8c236db32
juliana-manaloto <juliana.manaloto@benilde.edu.ph>
2025-04-08 22:05:43 +0800

    quiz6

---
+++ Quiz _6 Trees/Quiz_6 trees/src/Tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+    private Node root;
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new Node(value);
+            return;
+        }
+
+        root.insert(value);
+    }
+}
